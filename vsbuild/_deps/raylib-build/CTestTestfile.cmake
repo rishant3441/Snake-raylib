431ec6f40a28b45bf04891e06636d89d89313d7e
rambo3441 <31995864+rambo3441@users.noreply.github.com>
2021-09-27 20:56:18 -0400

    .gitignore is now working

--- vsbuild/_deps/raylib-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Dev/CLionProjects/Snake-raylib/vsbuild/_deps/raylib-src
-# Build directory: C:/Dev/CLionProjects/Snake-raylib/vsbuild/_deps/raylib-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("raylib")
--- vsbuild2/build/Mingw64-Debug/_deps/raylib-build/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Dev/CLionProjects/Snake-raylib/vsbuild2/build/Mingw64-Debug/_deps/raylib-src/src
-# Build directory: C:/Dev/CLionProjects/Snake-raylib/vsbuild2/build/Mingw64-Debug/_deps/raylib-build/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("external/glfw")
